ff5e23039b935ae286c74afc3d94d7caaef4f68f
Hao Ong <haoong@hong-mbp115.sfhq.lyft-corp.net>
2017-09-30 14:28:44 -0700

    add simulation for avg rewards after each episode

--- deeprl/agents/core.py
+++ deeprl/agents/core.py
@@ -19,7 +19,8 @@ class Agent(object):
               n_episodes,
               verbose=1,
               action_repetition=1,
-              max_episode_steps=float('inf')):
+              max_episode_steps=float('inf'),
+              n_simulations=0):
 
         start_time = timeit.default_timer()
         is_aborted = False
@@ -28,6 +29,9 @@ class Agent(object):
             'n_episode_steps': [],
             'episode_rewards': [],
         }
+
+        if n_simulations > 0:
+            history['avg_rewards'] = []
 
         episode = 0
         episode_step = 0
@@ -97,16 +101,83 @@ class Agent(object):
                 history['n_episode_steps'].append(episode_step)
                 history['episode_rewards'].append(episode_reward)
 
+                # simulate current agent for average rewards
+                if n_simulations > 0:
+                    simulation = 0
+                    simulation_rewards = 0.0
+
+                    while simulation < n_simulations:
+                        # initialize environment
+                        sim_observation = deepcopy(env.reset())
+                        if self.processor is not None:
+                            sim_observation = \
+                                self.processor.process_observation(
+                                    sim_observation
+                                )
+                        assert sim_observation is not None
+
+                        sim_done = False
+                        sim_episode_reward = 0.0
+                        sim_episode_step = 0
+
+                        while not sim_done and \
+                              sim_episode_step < max_episode_steps:
+                            sim_observation = deepcopy(sim_observation)
+                            sim_episode_step_reward = 0.0
+
+                            # select action
+                            sim_action = self.act(
+                                sim_observation,
+                                is_train=False
+                            )
+                            if self.processor is not None:
+                                sim_action = self.processor.process_action(
+                                    sim_action
+                                )
+
+                            # execute action
+                            for _ in xrange(action_repetition):
+                                sim_observation, sim_reward, sim_done, _ = \
+                                    env.step(sim_action)
+                                sim_observation = deepcopy(sim_observation)
+
+                                if self.processor is not None:
+                                    sim_observation = \
+                                        self.processor.process_observation(
+                                            sim_observation
+                                        )
+                                    sim_reward = self.process_reward(sim_reward)
+
+                                sim_episode_step_reward += sim_reward
+
+                                if sim_done:
+                                    break
+
+                            # housekeeping for each simulation step
+                            sim_episode_reward += sim_episode_step_reward
+                            sim_episode_step += 1
+
+                        # housekeeping for each simulation
+                        simulation_rewards += sim_episode_reward
+                        simulation += 1
+
+                    # housekeeping for simulations
+                    history['avg_rewards'].append(
+                        simulation_rewards / simulation
+                    )
+
                 episode += 1
                 observation = None
                 episode_reward = None
                 episode_step = None
 
                 if verbose:
-                    print("episode={}/{}: episode_reward={}".format(
+                    print("episode={}/{}: episode_reward={}{}".format(
                         episode,
                         n_episodes,
-                        history['episode_rewards'][-1]
+                        history['episode_rewards'][-1],
+                        ", avg_reward={}".format(history['avg_rewards'][-1]) \
+                            if n_simulations > 0 else ""
                     ))
 
         except KeyboardInterrupt:
